abdc600687818c0571cc674f6290aa477a2d5c92
mahedjaved <mahed95@gmail.com>
2022-08-21 07:22:27 +0100

    fixing final soln

--- wk12_uniqueInOrder.js
+++ wk12_uniqueInOrder.js
@@ -45,3 +45,90 @@
 
 // Algol 1 :: check for repittion
 // instead of starting from the begining of final array, this should be done instead with next
+const uniqueInOrder = (inArr) => {
+	let finalArr = [];
+	// const inArr = "AAAABBBCCDAABBB";
+	// const inArr = "ABBCcAD";
+	// const inArr = 12333314445566111;
+
+	// for the empty case attempt
+	if (inArr.length === 0) {
+		return [];
+	}
+	// make sure the input is string
+	const inputType = typeof inArr;
+	let newInput = inputType !== "string" ? inArr.toString() : inArr;
+
+	// remove the commas from the string
+	if (newInput.indexOf(",") > -1) {
+		newInput = newInput.replace(/\s/g, "").replaceAll(",", "");
+	}
+	// you need a detect change app
+	const detChange = (prev, curr) => {
+		if (prev === curr) {
+			return false;
+		} else {
+			return true;
+		}
+	};
+
+	finalArr.push(newInput[0]);
+	for (let i = 1; i < newInput.length; i++) {
+		if (detChange(newInput[i - 1], newInput[i])) {
+			// if change detected push the current one
+			finalArr.push(newInput[i]);
+		} else {
+			// otherwise just continue
+			continue;
+		}
+	}
+
+	if (inputType === "number") {
+		return finalArr.map((x) => {
+			return Number(x);
+		});
+	} else {
+		return finalArr;
+	}
+};
+console.log(uniqueInOrder("1111 , 2222 ,3333"));
+
+// var uniqueInOrder=function(iterable){
+// 	//your code here - remember iterable can be a string or an array
+// 	let finalArr = [];
+// 	  // const inArr = "AAAABBBCCDAABBB";
+// 	  // const inArr = "ABBCcAD";
+// 	  // const inArr = 12333314445566111;
+
+// 	  // make sure the input is string
+// 	  const inputType = typeof iterable;
+// 	  const newInput = inputType !== "string" ? iterable.toString() : iterable;
+
+// 	  // you need a detect change app
+// 	  const detChange = (prev, curr) => {
+// 		  if (prev === curr) {
+// 			  return false;
+// 		  } else {
+// 			  return true;
+// 		  }
+// 	  };
+
+// 	  finalArr.push(newInput[0]);
+// 	  for (let i = 1; i < newInput.length; i++) {
+// 		  if (detChange(newInput[i - 1], newInput[i])) {
+// 			  // if change detected push the current one
+// 			  finalArr.push(newInput[i]);
+// 		  } else {
+// 			  // otherwise just continue
+// 			  continue;
+// 		  }
+// 	  }
+
+// 	  if (inputType === "number") {
+// 		  return finalArr.map((x) => {
+// 			  return Number(x);
+// 		  });
+// 	  } else {
+// 		  return finalArr;
+// 	  }
+//   }
