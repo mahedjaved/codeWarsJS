46501da08f638dba9957377fad812a2c39afa438
Mahed <mahed95@gmail.com>
2022-08-29 10:02:33 +0100

    prime finished but shows error memory

--- wk13_prime.js
+++ wk13_prime.js
@@ -7,59 +7,91 @@
 // You can not assume that the integer will be only positive. You may be given negative numbers as well ( or 0 ).
 // NOTE on performance: There are no fancy optimizations required, but still the most trivial solutions might time out. Numbers go up to 2^31 ( or similar, depending on language ). Looping all the way up to n, or n/2, will be too slow.
 
+// ============================= DETAILED ANSWER ============================================ //
+
 // COND ##1 Cannot be zero -- Has to be a natural number, spawns 2 funcs below
 
-const checkIfZero = (inputInt) => {
-	if (inputInt === 0) {
-		throw "[info] The input cannot be a zero";
-	}
-};
+// const checkIfZero = (inputInt) => {
+// 	if (inputInt === 0) {
+// 		throw "[info] The input cannot be a zero";
+// 	}
+// };
 
-const takeOutFirst2 = (inputInt) => {
-	inputInt.splice(0, 2);
-};
+// const takeOutFirst2 = (inputInt) => {
+// 	inputInt.splice(0, 2);
+// };
 
-const checkIfNegative = (inputInt) => {
-	if (inputInt > 0) {
-		return false;
-	} else {
+// const checkIfNegative = (inputInt) => {
+// 	if (inputInt > 0) {
+// 		return false;
+// 	} else {
+// 		return true;
+// 	}
+// };
+
+// const checkIfWhole = (currNum, inputInt, verbose = false) => {
+// 	const result = inputInt / currNum;
+// 	if (verbose) {
+// 		console.log(
+// 			`[info func=checkIfWhole] Dividing ${inputInt} with ${currNum} gets the result ${result}, is it whole ? ${Number.isInteger(
+// 				result
+// 			)}`
+// 		);
+// 	}
+
+// 	return Number.isInteger(result);
+// };
+
+// // 1) Set the number, check if it zero, takeout the first two elements, proceed ...
+// const inNumber = -2;
+// checkIfZero(inNumber);
+// // "ARRAY-ify" all the integers that preceed this number
+// const nums2inNumber = [...Array(inNumber).keys()];
+// takeOutFirst2(nums2inNumber);
+// // console.log(`[info] Now inspecting the following : ${nums2inNumber}`);
+// let isNotPrimeNo = false;
+
+// // 2) Loop through this array
+// for (let i = 0; i < nums2inNumber.length; i++) {
+// 	// 3) Check if it is divisible by the number while being whole
+// 	// if yes break, other wise continue
+// 	if (checkIfWhole((currNum = nums2inNumber[i]), (inputInt = inNumber))) {
+// 		console.log(`[info] No, ${inNumber} is NOT prime`);
+// 		isNotPrimeNo = true;
+// 		break;
+// 	} else {
+// 		continue;
+// 	}
+// }
+// // not prime number message
+// !isNotPrimeNo && console.log(`[info] Yes, ${inNumber} is prime`);
+
+// ============================================================================================= //
+
+// ============================= SUBMITTED SOLN. ============================================ //
+const inputNumber = 5099;
+
+const isPrimeOrNot = (num) => {
+	if (num > 1) {
+		if (num === 2) {
+			return true;
+		}
+		const nums2inNumber = [...Array(num).keys()];
+		nums2inNumber.splice(0, 2);
+		// console.log(`[info] Now inspecting the following : ${nums2inNumber}`);
+		for (let i = 0; i < nums2inNumber.length; i++) {
+			const result = num / nums2inNumber[i];
+			// console.log(`[info] Results ${result} and is whole number : ${Number.isInteger(result)}`);
+			if (Number.isInteger(result)) {
+				return false;
+			} else {
+				continue;
+			}
+		}
 		return true;
-	}
-};
-
-const checkIfWhole = (currNum, inputInt, verbose = false) => {
-	const result = inputInt / currNum;
-	if (verbose) {
-		console.log(
-			`[info func=checkIfWhole] Dividing ${inputInt} with ${currNum} gets the result ${result}, is it whole ? ${Number.isInteger(
-				result
-			)}`
-		);
-	}
-
-	return Number.isInteger(result);
-};
-
-// 1) Set the number, check if it zero, takeout the first two elements, proceed ...
-const inNumber = -2;
-checkIfZero(inNumber);
-// "ARRAY-ify" all the integers that preceed this number
-const nums2inNumber = [...Array(inNumber).keys()];
-takeOutFirst2(nums2inNumber);
-// console.log(`[info] Now inspecting the following : ${nums2inNumber}`);
-let isNotPrimeNo = false;
-
-// 2) Loop through this array
-for (let i = 0; i < nums2inNumber.length; i++) {
-	// 3) Check if it is divisible by the number while being whole
-	// if yes break, other wise continue
-	if (checkIfWhole((currNum = nums2inNumber[i]), (inputInt = inNumber))) {
-		console.log(`[info] No, ${inNumber} is NOT prime`);
-		isNotPrimeNo = true;
-		break;
 	} else {
-		continue;
+		return false;
 	}
-}
-// not prime number message
-!isNotPrimeNo && console.log(`[info] Yes, ${inNumber} is prime`);
+};
+
+console.log(isPrimeOrNot((inNum = inputNumber)));
