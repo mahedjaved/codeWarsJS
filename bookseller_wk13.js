d2f44839626247f4ac30e8a3d3d09c5f6a0bfaf7
Mahed <mahed95@gmail.com>
2022-08-29 14:13:49 +0100

    bookseller finally works

--- bookseller_wk13.js
+++ bookseller_wk13.js
@@ -15,31 +15,32 @@
 const listOfArt = ["BBAR 150", "CDXE 515", "BKWR 250", "BTSQ 890", "DRTY 600"];
 const listOfCat = ["A", "B", "C", "D"];
 
-let bookStorageDict = {};
-for (let i = 0; i < listOfCat.length; i++) {
-	bookStorageDict[String(listOfCat[i])] = 0;
-}
-const [code, number] = listOfArt[0].split(" ");
-
-for (let i = 0; i < listOfArt.length; i++) {
-	// split the entry into first letter, and the number
-	const [code, number] = listOfArt[i].split(" ");
-
-	// find if there is a matching code in the dict
-	if (code[0] in bookStorageDict) {
-		// if yes, then add the current value of this with the number
-		bookStorageDict[String(code[0])] =
-			parseInt(bookStorageDict[String(code[0])]) + parseInt(number);
-	} else {
-		// if no, add this entry into the key and then add the value
-		bookStorageDict[String(code[0])] = number;
+if (listOfArt.length === 0 || listOfCat.length === 0) {
+	return "";
+} else {
+	let bookStorageDict = {};
+	for (let i = 0; i < listOfCat.length; i++) {
+		bookStorageDict[String(listOfCat[i])] = 0;
 	}
-}
+	const [code, number] = listOfArt[0].split(" ");
 
-let string_result = "";
+	for (let i = 0; i < listOfArt.length; i++) {
+		// split the entry into first letter, and the number
+		const [code, number] = listOfArt[i].split(" ");
 
-for (i in bookStorageDict) {
-	string_result += `(${i} : ${bookStorageDict[String(i)]}) - `;
+		// find if there is a matching code in the dict
+		if (code[0] in bookStorageDict) {
+			// if yes, then add the current value of this with the number
+			bookStorageDict[String(code[0])] =
+				parseInt(bookStorageDict[String(code[0])]) + parseInt(number);
+		}
+	}
+
+	let string_result = "";
+
+	for (i in bookStorageDict) {
+		string_result += `(${i} : ${bookStorageDict[String(i)]}) - `;
+	}
+	string_result = string_result.slice(0, -3);
+	console.log(string_result);
 }
-string_result = string_result.slice(0, -2);
-console.log(string_result);
