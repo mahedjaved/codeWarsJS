d8002ee1485a21d2bf7d95889af5d0bd5f403be5
mahed_javed <mahed95@gmail.com>
2022-09-01 02:51:40 +0100

    added desc. for pagination

---
+++ pagination_wk13.js
@@ -0,0 +1,86 @@
+//  which is a utility class helpful for querying paging information related to an array.
+
+// INPUT : 1) an array of values
+//       : 2) an integer  // indicating how many items will be allowed per each page
+
+// N.B : The types of values contained within the collection/array are not relevant.
+
+class PaginationHelper {
+	constructor(collection, itemsPerPage) {
+		this.collection = collection;
+		this.itemsPerPage = itemsPerPage;
+	}
+
+	// Method 1 :
+	itemCount() {
+		/**
+		 * @dec : takes in an array of items and a integer indicating how many items fit within a single page
+		 * @args : void
+		 * @returns : (int) => no. of items within the entire collection
+		 * */
+		return this.collection.length;
+	}
+
+	// Method 2 :
+	pageCount() {
+		/**
+		 * @desc : returns the numner of pages the items in the collection will take
+		 * @args : void
+		 * @returns : (int) =>
+		 */
+		const pageCount = 1;
+		for (let i = 0; i < this.itemCount(); i = i + this.itemsPerPage) {
+			pageCount++;
+		}
+		return pageCount;
+	}
+}
+
+// For this exercise you will be strengthening your page-fu mastery. You will complete the PaginationHelper class, which is a utility class helpful for querying paging information related to an array.
+
+// The class is designed to take in an array of values and an integer indicating how many items will be allowed per each page. The types of values contained within the collection/array are not relevant.
+
+// The following are some examples of how this class is used:
+
+// var helper = new PaginationHelper(['a','b','c','d','e','f'], 4);
+// helper.pageCount(); //should == 2
+// helper.itemCount(); //should == 6
+// helper.pageItemCount(0); //should == 4
+// helper.pageItemCount(1); // last page - should == 2
+// helper.pageItemCount(2); // should == -1 since the page is invalid
+
+// // pageIndex takes an item index and returns the page that it belongs on
+// helper.pageIndex(5); //should == 1 (zero based index)
+// helper.pageIndex(2); //should == 0
+// helper.pageIndex(20); //should == -1
+// helper.pageIndex(-10); //should == -1
+
+// // TODO: complete this object/class
+
+// // The constructor takes in an array of items and a integer indicating how many
+// // items fit within a single page
+// function PaginationHelper(collection, itemsPerPage){
+
+// }
+
+// // returns the number of items within the entire collection
+// PaginationHelper.prototype.itemCount = function() {
+
+// }
+
+// // returns the number of pages
+// PaginationHelper.prototype.pageCount = function() {
+
+// }
+
+// // returns the number of items on the current page. page_index is zero based.
+// // this method should return -1 for pageIndex values that are out of range
+// PaginationHelper.prototype.pageItemCount = function(pageIndex) {
+
+// }
+
+// // determines what page an item is on. Zero based indexes
+// // this method should return -1 for itemIndex values that are out of range
+// PaginationHelper.prototype.pageIndex = function(itemIndex) {
+
+// }
